7794474719e9e3343877441a43f7917ef2db3aa4
Daniel Gonzalez Fernandez <eldano1995@gmail.com>
2021-02-22 14:11:04 -0500

    docs: Add docstring to methods in EventBus

--- events_library/event_bus.py
+++ events_library/event_bus.py
@@ -8,27 +8,35 @@ EVENTS_URL = settings.EVENTS_URL
 
 
 class EventBus():
-    subscribers = {}
+    # A mapping, where the key is an event_type,
+    # and the value is a list of event_handlers
+    event_subscribers = {}
 
     @classmethod
     def subscribe(cls, event_type: str, event_handler: typing.Callable):
+        """Adds the event_handler to the list of functions to be
+        called when an event with the given event_type is emitted"""
         # Get current list of handlers
-        event_handlers = cls.subscribers.get(event_type, [])
+        event_handlers = cls.event_subscribers.get(event_type, [])
+
         # Add the new handler
         event_handlers.append(event_handler)
-
-        cls.subscribers[event_type] = event_handlers
+        cls.event_subscribers[event_type] = event_handlers
 
     @classmethod
     def emit_locally(cls, event_type: str, payload: typing.Dict):
-        if event_type not in cls.subscribers:
+        """Calls, with the given payload as argument, each
+        event_handler that was attached to the given event_type"""
+        if event_type not in cls.event_subscribers:
             return  # No op
 
-        for event_handler in cls.subscribers[event_type]:
+        for event_handler in cls.event_subscribers[event_type]:
             event_handler(payload)
 
     @classmethod
     def emit_abroad(cls, event_type: str, payload: typing.Dict):
+        """Sends the event to the services that were set (in the devops
+        submodule settings) as 'subscribed' to the given event_type"""
         if event_type not in EVENTS_MAPPING:
             return  # No op
 
