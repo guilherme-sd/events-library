a30685641f86678f78e76c08b1cc5f66aeb9d86d
Daniel Gonzalez Fernandez <eldano1995@gmail.com>
2021-02-22 17:39:00 -0500

    feat: Rename Event to EventLog

--- events_library/domain/event_log.py
+++ events_library/domain/event_log.py
@@ -1,5 +1,3 @@
-
-
 from django.db import models
 from .base import BaseModel
 
@@ -8,12 +6,13 @@ from django.contrib.postgres.fields import JSONField
 
 class EventLog(BaseModel):
 
+    target_service = models.CharField(editable=False)
     event_type = models.CharField(blank=False)
     payload = JSONField(default=dict)
-    target_service = models.CharField(editable=False)
 
-    retries = models.IntegerField(default=0)
-    success = models.BooleanField(default=False)
+    retry_number = models.IntegerField(default=0)
+    was_success = models.BooleanField(default=False)
+    error_message = models.TextField(blank=True)
 
     class Meta:
         abstract = True
