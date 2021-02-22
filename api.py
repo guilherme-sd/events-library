3c9f4fc963dc5e9bd12f412a6c50c36819c4a03d
Daniel Gonzalez Fernandez <eldano1995@gmail.com>
2021-02-22 11:09:14 -0500

    refactor: Remove unneeded logic from ApiBase

--- api.py
+++ api.py
@@ -1,63 +1,41 @@
 """Generic API class."""
-import enum
 import typing
 
-import requests
 from django.conf import settings
-
-
-@enum.unique
-class HttpMethod(enum.Enum):
-    """Basic HTTP methods."""
-
-    GET = 'GET'
-    POST = 'POST'
-    PUT = 'PUT'
-    PATCH = 'PATCH'
-    DELETE = 'DELETE'
+from requests import Request, RequestException, Session
+from rest_framework.renderers import JSONRenderer
 
 
 class BaseApi:
     """Base class for serving different APIs."""
 
-    domain = settings.DOMAIN_NAME
-
-    class Error(Exception):
-        """Generic error class."""
-
-        pass
-
     def __init__(self, domain: str = None) -> None:
         """Initialize requests session."""
-        self.session = requests.Session()
+        self.session = Session()
         self.session.headers.update({
             'Content-Type': 'application/json',
         })
-        self.domain = domain or self.domain
-
-    @property
-    def root_url(self) -> str:
-        """Return SD base path."""
-        return f'https://{self.domain}/'
+        self.domain = domain or settings.DOMAIN_NAME
 
     def send_request(
-            self, *,
-            method: HttpMethod,
-            path: str,
-            data: typing.Union[str, bytes] = None,
-            headers: typing.Dict[str, str] = None,
-            raise_exception: bool = False,
-    ) -> requests.Response:
+        self,
+        path: str,
+        data: typing.Dict,
+        raise_exception: bool = False,
+    ) -> typing.Tuple[int, bool]:
         """Send request function."""
-        req = requests.Request(
-            method=method.value,
-            url=f'{self.root_url}{path}',
-            headers=headers,
-            data=data,
+
+        req = Request(
+            method='POST',
+            url=f'https://{self.domain}/{path}',
+            data=JSONRenderer().render(data),
+            headers={
+                'Token': settings.JWT_AUTH['SERVICE_SECRET_TOKEN']
+            },
         )
+
         prepared_req = self.session.prepare_request(req)
 
         resp = self.session.send(prepared_req)
         if raise_exception:
             resp.raise_for_status()
-        return resp
