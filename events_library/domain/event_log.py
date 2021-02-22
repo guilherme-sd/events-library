56ed4950a95394515f7a56aee58c9c45bef693b3
Daniel Gonzalez Fernandez <eldano1995@gmail.com>
2021-02-22 17:49:39 -0500

    feat: Create HandlerLog model

--- events_library/domain/event_log.py
+++ events_library/domain/event_log.py
@@ -5,7 +5,6 @@ from django.contrib.postgres.fields import JSONField
 
 
 class EventLog(BaseModel):
-
     target_service = models.CharField(editable=False)
     event_type = models.CharField(blank=False)
     payload = JSONField(default=dict)
@@ -13,6 +12,3 @@ class EventLog(BaseModel):
     retry_number = models.IntegerField(default=0)
     was_success = models.BooleanField(default=False)
     error_message = models.TextField(blank=True)
-
-    class Meta:
-        abstract = True
