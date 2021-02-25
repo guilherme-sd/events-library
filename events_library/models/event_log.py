db61a69bbf1a9378741059242c96661a94802240
Daniel Gonzalez Fernandez <eldano1995@gmail.com>
2021-02-24 23:44:17 -0500

    feat: Create migrations for EventLog and HandlerLog models

--- events_library/models/event_log.py
+++ events_library/models/event_log.py
@@ -5,8 +5,8 @@ from django.contrib.postgres.fields import JSONField
 
 
 class EventLog(BaseModel):
-    target_service = models.CharField(editable=False)
-    event_type = models.CharField(blank=False)
+    target_service = models.CharField(max_length=20, editable=False)
+    event_type = models.CharField(max_length=60, blank=False)
     payload = JSONField(default=dict)
 
     retry_number = models.IntegerField(default=0)
--- events_library/models/handler_log.py
+++ events_library/models/handler_log.py
@@ -5,8 +5,8 @@ from django.contrib.postgres.fields import JSONField
 
 
 class HandlerLog(BaseModel):
-    handler_name = models.CharField()
+    handler_name = models.CharField(max_length=60)
     error_message = models.TextField()
 
-    event_type = models.CharField(blank=False)
+    event_type = models.CharField(max_length=60, blank=False)
     payload = JSONField(default=dict)
