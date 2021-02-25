001143462700db2e42580ca6a5fca04311206a5d
Daniel Gonzalez Fernandez <eldano1995@gmail.com>
2021-02-25 12:53:03 -0500

    feat: Disable add and edit permissions for HandlerLog and EventLog models in admin

--- events_library/admin.py
+++ events_library/admin.py
@@ -1,6 +1,24 @@
+import typing
+
 from django.contrib import admin
+from django.db.models import Model
+from django.http.request import HttpRequest
+
 from events_library.models import EventLog, HandlerLog
 
+
+class InmutableAdminModel(admin.ModelAdmin):
+    def has_add_permission(self, request: HttpRequest) -> bool:
+        return False
+
+    def has_change_permission(
+        self,
+        request: HttpRequest,
+        obj: typing.Optional[Model] = None,
+    ) -> bool:
+        return False
+
+
 # Register your models here.
-admin.site.register(EventLog)
-admin.site.register(HandlerLog)
+admin.site.register(EventLog, InmutableAdminModel)
+admin.site.register(HandlerLog, InmutableAdminModel)
