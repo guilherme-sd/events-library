dfd31b43e799b643b7e70e5f0b458c11bfc9d009
Daniel Gonzalez Fernandez <eldano1995@gmail.com>
2021-02-26 16:08:52 -0500

    feat: Update EventViewSet to handle cud events

--- events_library/application/views.py
+++ events_library/application/views.py
@@ -7,7 +7,9 @@ from rest_framework.status import HTTP_204_NO_CONTENT
 from jwt_auth.authentication import ServiceTokenAuthentication
 
 from events_library.application.permissions import ServiceTokenPermission
-from events_library.application.serializers import EventSerializer
+from events_library.application.serializers import (
+    EventSerializer, CudPayloadSerializer,
+)
 from events_library.core.event_bus import EventBus
 
 
@@ -18,12 +20,16 @@ class EventViewSet(ViewSet):
 
     @action(detail=False, methods=['post'], url_path='event')
     def handle_event(self, request: Request):
-        serializer = EventSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
+        event_serializer = EventSerializer(data=request.data)
+        event_serializer.is_valid(raise_exception=True)
 
-        event_type = serializer.validated_data['event_type']
-        payload = serializer.validated_data['payload']
+        event_type = event_serializer.validated_data['event_type']
+        payload = event_serializer.validated_data['payload']
 
-        EventBus.emit_locally(event_type, payload)
+        payload_serializer = CudPayloadSerializer(data=payload)
+        if payload_serializer.is_valid():
+            EventBus.emit_cud_locally(event_type, payload)
+        else:
+            EventBus.emit_locally(event_type, payload)
 
         return Response(status=HTTP_204_NO_CONTENT)
