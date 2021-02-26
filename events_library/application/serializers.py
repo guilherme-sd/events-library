3759d06e5c7e9ac992546e3387974baf856344d3
Daniel Gonzalez Fernandez <eldano1995@gmail.com>
2021-02-26 15:51:00 -0500

    feat: Create CudPayloadSerializer

--- events_library/application/serializers.py
+++ events_library/application/serializers.py
@@ -4,3 +4,16 @@ from rest_framework import serializers
 class EventSerializer(serializers.Serializer):
     event_type = serializers.CharField()
     payload = serializers.JSONField()
+
+
+class CudPayloadSerializer(serializers.Serializer):
+    id = serializers.UUIDField()
+    cud_operation = serializers.CharField()
+    data = serializers.JSONField(),
+    timestamp = serializers.DateTimeField()
+
+    class Meta:
+        fields = '__all__'
+
+    def validate_cud_operation(self, value):
+        return value in ['created', 'updated', 'deleted']
